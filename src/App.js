354a3a441431520d92c889ddb0ee5438fd1ffd5a
pooreum <vnfma0218@naver.com>
2021-11-25 18:16:25 +0900

    initialize prj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    // if (todo.trim().length > 0) return;
+    setTodos((prev) => {
+      return [...prev, { id: Math.random(), todo: todo }];
+    });
+    setTodo('');
+  };
+  const removeTodoHandler = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h2>To Do</h2>
+        <form onSubmit={addTodoHandler}>
+          <input
+            type='text'
+            id='todo'
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          />
+          <button>제출</button>
+        </form>
       </header>
+      <section className='todos'>
+        <ul className='todo-list'>
+          {todos.map((todo) => (
+            <li className='todo-item' key={todo.id}>
+              <p>{todo.todo}</p>
+              <span
+                onClick={() => {
+                  removeTodoHandler(todo.id);
+                }}
+              >
+                ❌
+              </span>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
